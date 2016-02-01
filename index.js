895088af42080a43e3f753774f5d24ae160ec205
Alexander <alykoshin@gmail.com>
2016-02-01 14:43:18 +0300

    tests passed

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
 'use strict';
 
-module.exports = require('./lib/index');
+//var parentFile;
+//var parentDir;
+
+module.exports = function getParent(mod, _parentsToSkip) {
+  _parentsToSkip = _parentsToSkip || 0;
+
+  var callingModule = mod;
+  for (var i=0; i <= _parentsToSkip; i++) {
+    ////////////////////////////////////////////////////////////////////////////////
+    // Trick taken from https://github.com/aseemk/requireDir/blob/master/index.js //
+    //                                                                            //
+    // make a note of the calling file's path, so that we can resolve relative    //
+    // paths. this only works if a fresh version of this module is run on every   //
+    // require(), so important: we clear the require() cache each time!           //
+    //                                                                            //
+    delete require.cache[ callingModule.filename ];
+    //                                                                            //
+    ////////////////////////////////////////////////////////////////////////////////
+    callingModule = callingModule.parent;
+  }
+
+  return callingModule;
+  //parentFile   = callingModule.filename;
+  //parentDir    = path.dirname(parentFile);
+
+
+  //////////////////////////////////////////////////////////////////////////////////
+  //// Trick taken from https://github.com/aseemk/requireDir/blob/master/index.js //
+  ////                                                                            //
+  //// make a note of the calling file's path, so that we can resolve relative    //
+  //// paths. this only works if a fresh version of this module is run on every   //
+  //// require(), so important: we clear the require() cache each time!           //
+  ////                                                                            //
+  //delete require.cache[ __filename ];
+  ////                                                                            //
+  //////////////////////////////////////////////////////////////////////////////////
+  //
+  //var parentModule = module.parent;
+  //var parentFile   = parentModule.filename;
+  //var parentDir    = path.dirname(parentFile);
+}
 
--- test/index.js
+++ test/index.js
@@ -4,31 +4,63 @@
 
 var chai = require('chai'),
     assert = chai.assert,
-    expect = chai.expect,
-    should = chai.should();
+    expect = chai.expect;
+chai.should();
+chai.use(require('chai-things'));
+
+var path = require('path');
+
+
+var moduleParent = require('../');
 
 describe('module-parent', function () {
 
+  var parent0 = moduleParent(module, 0);
+  var basename0 = path.basename(parent0.filename);
+
+  var parent1 = moduleParent(module, 1);
+  var basename1 = path.basename(parent1.filename);
+
+  var parent2 = moduleParent(module, 2);
+  var basename2 = path.basename(parent2.filename);
+
   before('before', function () {
-
   });
 
-  beforeEach('before', function () {
 
+  it('expect(basename0).to.be.equal(\'mocha.js\')s', function () {
+    expect(basename0).to.be.equal('mocha.js');
   });
 
-  afterEach('after', function () {
-
+  it('expect(parent0.parent).to.be.deep.equal(parent1)', function () {
+    expect(parent0.parent).to.be.deep.equal(parent1);
   });
 
-  after('after', function () {
-
+  it('expect(parent0.children).to.include.something.that.equals(module)', function () {
+    expect(parent0.children).to.include.something.that.equals( module );
   });
 
-  it('should have unit test', function () {
 
-    assert(false, 'Please add unit tests.');
+  it('expect(basename1).to.be.equal(\'index.js\')', function () {
+    expect(basename1).to.be.equal('index.js');
+  });
 
+  it('expect(parent1.parent).to.be.equal(parent2)', function () {
+    expect(parent1.parent).to.be.equal(parent2);
+  });
+
+  it('expect(parent1.children).to.include.something.that.equals( parent0 )', function () {
+    expect(parent1.children).to.include.something.that.equals( parent0 );
+  });
+
+
+  it('expect(basename2).to.be.equal(\'_mocha\')', function () {
+    expect(basename2).to.be.equal('_mocha');
+  });
+
+  it('expect(parent2.children).to.include.something.that.equals( parent1 )', function () {
+    expect(parent2.children).to.include.something.that.equals( parent1 );
   });
 
 });
+//expect(parent.children).to.contain.a.thing.with.property('id', module.filename);
